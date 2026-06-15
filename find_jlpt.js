969985179c081787e21f9c563985c60c74f4adc2
NganNNK986 <ngannnk986@gmail.com>
2026-06-15 10:11:03 +0700

    refactor: organize and move data management scripts into a dedicated directory

--- find_jlpt.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('old_vocab.js', 'utf16le');
-const jlpt = [...content.matchAll(/id:\s*"v10(\d{2})"/g)];
-console.log('Found:', jlpt.length);
